34a79d7d87332dc2a90c39f54b6f497d818fcf3b
WojtekM <1062789+poppyseedcake@users.noreply.github.com>
2024-12-27 00:42:11 +0100

    dasdsa

--- calc.js
+++ calc.js
@@ -13,3 +13,26 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, operator, num2) {
+    let result = 0;
+    switch(operator){
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+    }
+    return result;
+}
+
+let num1;
+let num2;
+let operator;
